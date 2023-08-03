83a095d634464ae4d38d88813ae200fa716328a1
Abdel004 <1155129200@link.cuhk.edu.hk>
2023-08-03 17:29:18 +0300

    final touches to application

--- src/App.js
+++ src/App.js
@@ -2,55 +2,16 @@ import SearchForm from './components/SearchForm/SearchForm';
 import ResultsContainer from './components/ResultsContainer/ResultsContainer';
 import PlaylistContainer from './components/PlaylistContainer/PlaylistContainer';
 import styles from './App.module.css';
-import { RequestAccessToken } from './modules/utils';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Spotify from './modules/Spotify';
 
 function App() {
 
-  useEffect(() => {
-    let accessToken = localStorage.getItem('access_token');
-    if (!accessToken) {
-      RequestAccessToken()
-    }
-
-  }, [])
-
-
-
   const [tracklist, setTracklist] = useState([
-    {
-      title: "Leave The Door Open",
-      artist: "Silk Sonic",
-      album: "An Evening with Silk Sonic",
-      uri: "spotify:track:7MAibcTli4IisCtbHKrGMh"
-    },
-    {
-      title: "Good 4 U",
-      artist: "Olivia Rodrigo",
-      album: "SOUR",
-      uri: "spotify:track:4ZtFanR9U6ndgddUvNcjcG"
-    },
-    {
-      title: "Montero (Call Me By Your name)",
-      artist: "Lil Nas X",
-      album: "Montero",
-      uri: "spotify:track:67BtfxlNbhBmCDR2L2l8qd"
-    }
   ])
 
   const [playlist, setPlaylist] = useState([
-    {
-      title: "Blinding Lights",
-      artist: "The Weeknd",
-      album: "After Hours",
-      uri: "spotify:track:0VjIjW4GlUZAMYd2vXMi3b"
-    },
-    {
-      title: "drivers license",
-      artist: "Olivia Rodrigo",
-      album: "SOUR",
-      uri: "spotify:track:7lPN2DXiMsVn7XUKtOW1CS"
-    },
+
   ])
 
   const handleAdditionToPlaylist = (song) => {
@@ -77,31 +38,13 @@ function App() {
       }
     });
 
-    const data = await response.json()
-    const trackList = data.tracks.items.map(item => {
-      const artists = item.artists.map(artist => {
-        return artist.name
-      })
-      let artist;
-      if (artists.length > 1) {
-        artist = artists.join(" & ")
+    const tracklist = await Spotify.search(searchQuery)
+    setTracklist(tracklist)
+  }
 
-      } else {
-        artist = artists[0]
-      }
-
-      // console.log(artist)
-
-      return {
-        uri: item.uri,
-        title: item.name,
-        artist: artist,
-        album: item.album.name,
-      }
-
-    })
-
-    setTracklist(trackList)
+  const saveSpotifyPlayList = (playlistTitle) => {
+    const URIS = playlist.map(track => track.uri)
+    Spotify.savePlaylist(playlistTitle, URIS)
   }
 
   return (
@@ -109,7 +52,7 @@ function App() {
       <SearchForm searchForResults={fetchSpotifyResults} />
       <div className={styles.resultContainer}>
         <ResultsContainer trackList={tracklist} addToPlaylist={handleAdditionToPlaylist} />
-        <PlaylistContainer playList={playlist} removeFromTracklist={handleRemovalFromTracklist} />
+        <PlaylistContainer playList={playlist} removeFromTracklist={handleRemovalFromTracklist} savePlaylist={saveSpotifyPlayList}/>
       </div>
     </div>
   );
--- src/components/PlaylistContainer/PlaylistContainer.js
+++ src/components/PlaylistContainer/PlaylistContainer.js
@@ -3,16 +3,22 @@ import styles from './PlaylistContainer.module.css';
 import SongCard from '../SongCard/SongCard';
 
 
-function PlaylistContainer({ playList, removeFromTracklist }) {
+function PlaylistContainer({ playList, removeFromTracklist, savePlaylist }) {
 
+    const [playlistTitle, setPlaylistTitle] = useState('')
+
+    const handleChange = (event) => {
+        setPlaylistTitle(event.target.value)
+    }
 
     const addPlaylistToSpotify = (event) => {
-
+        savePlaylist(playlistTitle)
+        setPlaylistTitle('')
     }
-    
+
     return (
         <div className={styles.resultCard}>
-            <h1><input className={styles.playlistInput} placeholder='Enter playlist name' /></h1>
+            <h1><input className={styles.playlistInput} placeholder='Enter playlist name' value={playlistTitle} onChange={handleChange} /></h1>
             {playList.map(song => {
                 return <SongCard key={song.uri} uri={song.uri} title={song.title} album={song.album} artist={song.artist} type="playList" handleButtonClick={removeFromTracklist} />
             })}
